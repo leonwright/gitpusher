770789bfc6d40a0cb94da8719c3d6fff7c088510
leonwright <wrightleon@outlook.com>
2021-03-22 23:10:53 -0500

    debug

--- cmd/gitpshr/gitpshr.go
+++ cmd/gitpshr/gitpshr.go
@@ -8,15 +8,55 @@ import (
 	"io"
 	"log"
 	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	ctx := context.Background()
-	c := &config.RunConfig{}
-	if err := run(ctx, c, os.Stdout); err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		os.Exit(1)
+	// ctx := context.Background()
+	// c := &config.RunConfig{}
+	// if err := run(ctx, c, os.Stdout); err != nil {
+	// 	fmt.Fprintf(os.Stderr, "%s\n", err)
+	// 	os.Exit(1)
+	// }
+	app := &cli.App{
+		Name:  "boom",
+		Usage: "make an explosive entrance",
+		Action: func(c *cli.Context) error {
+			fmt.Println("boom! I say!")
+			return nil
+		},
+		Commands: []*cli.Command{
+			{
+				Name:    "savefile",
+				Aliases: []string{"c"},
+				Usage:   "commit a file and push to remote",
+				Action: func(c *cli.Context) error {
+					var commitCmd = []string{"git", "commit", "-a"}
+
+					commitCmd = append(commitCmd, "-m")
+					commitCmd = append(commitCmd, "debug")
+
+					// testCmd := []string{"tr", "a-z", "A-Z"}
+
+					cmd := commander.Command{Parts: commitCmd}
+					cmd.RunCommand()
+
+					pushCmd := []string{"git", "push", "origin", "master"}
+
+					pshCmd := commander.Command{Parts: pushCmd}
+					pshCmd.RunCommand()
+					return nil
+				},
+			},
+		},
 	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
 
 func run(ctx context.Context, c *config.RunConfig, out io.Writer) error {
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module gitpusher
 
 go 1.16
 
-require (
-	github.com/namsral/flag v1.7.4-pre
-	golang.org/x/tools v0.1.0 // indirect
-)
+require github.com/urfave/cli/v2 v2.3.0
